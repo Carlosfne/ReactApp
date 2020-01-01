ee20467978102f85789febe169f4b6fa537fb838
Carlos Eduardo Jr <carlos.moreira@mapinnovation.com.br>
2020-01-01 17:57:49 -0400

    Application in ReactJs

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import "./styles.css"
+
+const Header = () => (
+  <header id="main-header">JSHunt</header>
+);
+// mesma coisa que ... 
+// class Header extends Component {
+//   render(){
+//     return <h1>Hello</h1>
+//   }
+// } utilizamos esta quando temos conceito de estado
+
+export default Header;
